6b8d7cde88231bd120c60459015a69e47d32824a
chenzili <chen__zili@163.com>
2023-06-30 10:27:21 +0800

    add field_one_side

--- test/field_one_side.F90
+++ test/field_one_side.F90
@@ -18,7 +18,6 @@ program field_nonblock__comm
     integer(4) :: send_buff_edge_right(1:Ly)
     integer(4) :: send_buff_edge_top(1:Lx)
     integer(4) :: send_buff_edge_bottom(1:Lx)
-    integer(4) :: send_buff_corner(4)
 
     integer(4) :: recv_buff_edge_left(1:Ly)
     integer(4) :: recv_buff_edge_right(1:Ly)
@@ -26,14 +25,9 @@ program field_nonblock__comm
     integer(4) :: recv_buff_edge_bottom(1:Lx)
     integer(4) :: recv_buff_corner(4)
 
-    integer(4) :: reqs_left(2), reqs_right(2), reqs_top(2), reqs_bottom(2)
-    integer(4) :: reqs_corner(8)
+    integer(4) :: win_left, win_right, win_top, win_bottom
+    integer(kind=MPI_ADDRESS_KIND) :: win_size, disp_aint
 
-    integer(4) :: status_left(MPI_STATUS_SIZE, 2), status_right(MPI_STATUS_SIZE, 2)
-    integer(4) :: status_top(MPI_STATUS_SIZE, 2), status_bottom(MPI_STATUS_SIZE, 2)
-    integer(4) :: status_corner(MPI_STATUS_SIZE, 8)
-
-    integer(4) :: recv_count, recv_count_corner(4)
     integer(4) :: size, rank, ierr
     integer(4) :: row, col, i, j
 
@@ -111,16 +105,25 @@ program field_nonblock__comm
         call MPI_BARRIER(MPI_COMM_WORLD, ierr)
     end do
 
+    ! 创建远程访问窗口
+    win_size = 4 * Ly
+    call MPI_WIN_CREATE(send_buff_edge_left, win_size, 4, MPI_INFO_NULL, MPI_COMM_WORLD, win_left, ierr)
+
+    win_size = 4 * Ly
+    call MPI_WIN_CREATE(send_buff_edge_right, win_size, 4, MPI_INFO_NULL, MPI_COMM_WORLD, win_right, ierr)
+
+    win_size = 4 * Lx
+    call MPI_WIN_CREATE(send_buff_edge_top, win_size, 4, MPI_INFO_NULL, MPI_COMM_WORLD, win_top, ierr)
+
+    win_size = 4 * Lx
+    call MPI_WIN_CREATE(send_buff_edge_bottom, win_size, 4, MPI_INFO_NULL, MPI_COMM_WORLD, win_bottom, ierr)
+
     ! 设置field和recv buffer
     field = 1
     send_buff_edge_left = field(1, 1:Ly)
     send_buff_edge_right = field(Lx, 1:Ly)
     send_buff_edge_top = field(1:Lx, 1)
     send_buff_edge_bottom = field(1:Lx, Ly)
-    send_buff_corner(1) = field(1, 1)
-    send_buff_corner(2) = field(Lx, 1)
-    send_buff_corner(3) = field(Lx, Ly)
-    send_buff_corner(4) = field(1, Ly)
 
     recv_buff_edge_left = 0
     recv_buff_edge_right = 0
@@ -128,101 +131,86 @@ program field_nonblock__comm
     recv_buff_edge_bottom = 0
     recv_buff_corner = 0
 
+    call MPI_BARRIER(MPI_COMM_WORLD, ierr)
+
     ! send and recv edge
     if (negb_type(8) == negb_type_domain) then  ! left
-        call MPI_ISEND(send_buff_edge_left, Ly, MPI_INTEGER, negb_rank(8), 1, MPI_COMM_WORLD, reqs_left(1), ierr)
-        call MPI_IRECV(recv_buff_edge_left, Ly, MPI_INTEGER, negb_rank(8), 1, MPI_COMM_WORLD, reqs_left(2), ierr)
+        disp_aint = 0
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(8), 0, win_left, ierr)
+        call MPI_GET(recv_buff_edge_left, Ly, MPI_INTEGER, negb_type(8), disp_aint, Ly, MPI_INTEGER, win_left, ierr)
+        call MPI_WIN_UNLOCK(negb_type(8), win_left, ierr)
+
+        field(1, 1:Ly) = field(1, 1:Ly) + recv_buff_edge_left
     end if
 
     if (negb_type(4) == negb_type_domain) then  ! right
-        call MPI_ISEND(send_buff_edge_right, Ly, MPI_INTEGER, negb_rank(4), 1, MPI_COMM_WORLD, reqs_right(1), ierr)
-        call MPI_IRECV(recv_buff_edge_right, Ly, MPI_INTEGER, negb_rank(4), 1, MPI_COMM_WORLD, reqs_right(2), ierr)
+        disp_aint = 0
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(4), 0, win_right, ierr)
+        call MPI_GET(recv_buff_edge_right, Ly, MPI_INTEGER, negb_type(4), disp_aint, Ly, MPI_INTEGER, win_right, ierr)
+        call MPI_WIN_UNLOCK(negb_type(4), win_right, ierr)
+
+        field(Lx, 1:Ly) = field(Lx, 1:Ly) + recv_buff_edge_right
     end if
 
     if (negb_type(2) == negb_type_domain) then  ! top
-        call MPI_ISEND(send_buff_edge_top, Lx, MPI_INTEGER, negb_rank(2), 1, MPI_COMM_WORLD, reqs_top(1), ierr)
-        call MPI_IRECV(recv_buff_edge_top, Lx, MPI_INTEGER, negb_rank(2), 1, MPI_COMM_WORLD, reqs_top(2), ierr)
+        disp_aint = 0
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(2), 0, win_top, ierr)
+        call MPI_GET(recv_buff_edge_top, Lx, MPI_INTEGER, negb_type(2), disp_aint, Lx, MPI_INTEGER, win_top, ierr)
+        call MPI_WIN_UNLOCK(negb_type(2), win_top, ierr)
+
+        field(1:Lx, 1) = field(1:Lx, 1) + recv_buff_edge_top
     end if
 
     if (negb_type(6) == negb_type_domain) then  ! bottom
-        call MPI_ISEND(send_buff_edge_bottom, Lx, MPI_INTEGER, negb_rank(6), 1, MPI_COMM_WORLD, reqs_bottom(1), ierr)
-        call MPI_IRECV(recv_buff_edge_bottom, Lx, MPI_INTEGER, negb_rank(6), 1, MPI_COMM_WORLD, reqs_bottom(2), ierr)
+        disp_aint = 0
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(6), 0, win_bottom, ierr)
+        call MPI_GET(recv_buff_edge_bottom, Lx, MPI_INTEGER, negb_type(6), disp_aint, Lx, MPI_INTEGER, win_bottom, ierr)
+        call MPI_WIN_UNLOCK(negb_type(6), win_bottom, ierr)
+
+        field(1:Lx, Ly) = field(1:Lx, Ly) + recv_buff_edge_bottom
     end if
 
     ! send and recv corner
     if (negb_type(1) == negb_type_domain) then ! corner left top
-        call MPI_ISEND(send_buff_corner(1), 1, MPI_INTEGER, negb_rank(1), 1, MPI_COMM_WORLD, reqs_corner(1), ierr)
-        call MPI_IRECV(recv_buff_corner(1), 1, MPI_INTEGER, negb_rank(1), 1, MPI_COMM_WORLD, reqs_corner(2), ierr)
+        disp_aint = Lx-1
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(1), 0, win_bottom, ierr)
+        call MPI_GET(recv_buff_corner(1), 1, MPI_INTEGER, negb_type(1), disp_aint, 1, MPI_INTEGER, win_bottom, ierr)
+        call MPI_WIN_UNLOCK(negb_type(1), win_bottom, ierr)
+
+        field(1, 1) = field(1, 1) + recv_buff_corner(1)
     end if
 
     if (negb_type(3) == negb_type_domain) then ! corner right top
-        call MPI_ISEND(send_buff_corner(2), 1, MPI_INTEGER, negb_rank(3), 1, MPI_COMM_WORLD, reqs_corner(3), ierr)
-        call MPI_IRECV(recv_buff_corner(2), 1, MPI_INTEGER, negb_rank(3), 1, MPI_COMM_WORLD, reqs_corner(4), ierr)
+        disp_aint = 0
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(3), 0, win_bottom, ierr)
+        call MPI_GET(recv_buff_corner(2), 1, MPI_INTEGER, negb_type(3), disp_aint, 1, MPI_INTEGER, win_bottom, ierr)
+        call MPI_WIN_UNLOCK(negb_type(3), win_bottom, ierr)
+
+        field(Lx, 1) = field(Lx, 1) + recv_buff_corner(2)
     end if
 
     if (negb_type(5) == negb_type_domain) then ! corner right bottom
-        call MPI_ISEND(send_buff_corner(3), 1, MPI_INTEGER, negb_rank(5), 1, MPI_COMM_WORLD, reqs_corner(5), ierr)
-        call MPI_IRECV(recv_buff_corner(3), 1, MPI_INTEGER, negb_rank(5), 1, MPI_COMM_WORLD, reqs_corner(6), ierr)
+        disp_aint = 0
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(5), 0, win_top, ierr)
+        call MPI_GET(recv_buff_corner(3), 1, MPI_INTEGER, negb_type(5), disp_aint, 1, MPI_INTEGER, win_top, ierr)
+        call MPI_WIN_UNLOCK(negb_type(5), win_top, ierr)
+
+        field(Lx, Ly) = field(Lx, Ly) + recv_buff_corner(3)
     end if
 
     if (negb_type(7) == negb_type_domain) then ! corner left bottom
-        call MPI_ISEND(send_buff_corner(4), 1, MPI_INTEGER, negb_rank(7), 1, MPI_COMM_WORLD, reqs_corner(7), ierr)
-        call MPI_IRECV(recv_buff_corner(4), 1, MPI_INTEGER, negb_rank(7), 1, MPI_COMM_WORLD, reqs_corner(8), ierr)
-    end if
+        disp_aint = Lx-1
+        call MPI_WIN_LOCK(MPI_LOCK_SHARED, negb_type(7), 0, win_top, ierr)
+        call MPI_GET(recv_buff_corner(4), 1, MPI_INTEGER, negb_type(7), disp_aint, 1, MPI_INTEGER, win_top, ierr)
+        call MPI_WIN_UNLOCK(negb_type(7), win_top, ierr)
 
-    ! wait and accumulation
-    recv_count_corner = 0
-    if (negb_type(8) == negb_type_domain) then  ! left
-        call MPI_WAITALL(2, reqs_left, status_left, ierr)
-        field(1, 1:Ly) = field(1, 1:Ly) + recv_buff_edge_left
-        recv_count_corner(1) = recv_count_corner(1) + 1
-        recv_count_corner(4) = recv_count_corner(4) + 1
-    end if
-
-    if (negb_type(4) == negb_type_domain) then  ! right
-        call MPI_WAITALL(2, reqs_right, status_right, ierr)
-        field(Lx, 1:Ly) = field(Lx, 1:Ly) + recv_buff_edge_right
-        recv_count_corner(2) = recv_count_corner(2) + 1
-        recv_count_corner(3) = recv_count_corner(3) + 1
-    end if
-
-    if (negb_type(2) == negb_type_domain) then  ! top
-        call MPI_WAITALL(2, reqs_top, status_top, ierr)
-        field(1:Lx, 1) = field(1:Lx, 1) + recv_buff_edge_top
-        recv_count_corner(1) = recv_count_corner(1) + 1
-        recv_count_corner(2) = recv_count_corner(2) + 1
-    end if
-
-    if (negb_type(6) == negb_type_domain) then  ! bottom
-        call MPI_WAITALL(2, reqs_bottom, status_bottom, ierr)
-        field(1:Lx, Ly) = field(1:Lx, Ly) + recv_buff_edge_bottom
-        recv_count_corner(3) = recv_count_corner(3) + 1
-        recv_count_corner(4) = recv_count_corner(4) + 1
-    end if
-
-    if (negb_type(1) == negb_type_domain) then  ! left top
-        call MPI_WAITALL(2, reqs_corner(1:2), status_corner(:, 1:2), ierr)
-        field(1, 1) = field(1, 1) + recv_buff_corner(1)
-        recv_count_corner(1) = recv_count_corner(1) + 1
-    end if
-    
-    if (negb_type(3) == negb_type_domain) then  ! right top
-        call MPI_WAITALL(2, reqs_corner(3:4), status_corner(:, 3:4), ierr)
-        field(Lx, 1) = field(Lx, 1) + recv_buff_corner(2)
-        recv_count_corner(2) = recv_count_corner(2) + 1
-    end if
-
-    if (negb_type(5) == negb_type_domain) then  ! right bottom
-        call MPI_WAITALL(2, reqs_corner(5:6), status_corner(:, 5:6), ierr)
-        field(Lx, Ly) = field(Lx, Ly) + recv_buff_corner(3)
-        recv_count_corner(3) = recv_count_corner(3) + 1
-    end if
-
-    if (negb_type(7) == negb_type_domain) then  ! left bottom
-        call MPI_WAITALL(2, reqs_corner(7:8), status_corner(:, 7:8), ierr)
         field(1, Ly) = field(1, Ly) + recv_buff_corner(4)
-        recv_count_corner(4) = recv_count_corner(4) + 1
     end if
+
+    call MPI_WIN_FREE(win_left, ierr)
+    call MPI_WIN_FREE(win_right, ierr)
+    call MPI_WIN_FREE(win_top, ierr)
+    call MPI_WIN_FREE(win_bottom, ierr)
 
     ! out result
     allocate(all_field(Lx*deps_col, Ly*size/deps_col))
@@ -234,7 +222,7 @@ program field_nonblock__comm
     call MPI_ALLREDUCE(all_field, field_out, Lx*Ly*size, MPI_INTEGER, MPI_SUM, MPI_COMM_WORLD, ierr)
 
     if (0 == rank) then
-        open(10, file="./result_field_nonblock.txt")
+        open(10, file="./result_field_one_side.txt")
             do i = 1, Ly*size/deps_col
                 write(10, '(*(i10, 1x))') field_out(:, i)
             end do
